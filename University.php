685804ca8863fc220e9befde6323ec49e2431d64
sharda-sahu21 <shardasahu1321@gmail.com>
2024-06-02 14:07:10 +0530

    Add files via upload

---
+++ University.php
@@ -0,0 +1,256 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>University Website</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
+
+  <!-- Favicons -->
+  <link href="" rel="icon">
+  <link href="img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Montserrat:300,400,500,700" rel="stylesheet">
+
+  <!-- Bootstrap CSS File -->
+  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Libraries CSS Files -->
+  <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="lib/animate/animate.min.css" rel="stylesheet">
+  <link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+  <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+
+  <!-- Main Stylesheet File -->
+  <link href="css/style.css" rel="stylesheet">
+  <img src="img/uni_web.jpg" alt="" class="img-fluid" style="
+     margin-top: -2px;
+    height: 39px;
+    width: 41px;
+    border-radius: 42px;
+
+    margin-left: -58px;">
+
+
+  <!-- =======================================================
+    Theme Name: NewBiz
+    Theme URL: https://bootstrapmade.com/newbiz-bootstrap-business-template/
+    Author: BootstrapMade.com
+    License: https://bootstrapmade.com/license/
+  ======================================================= -->
+</head>
+
+<body>
+
+  <!--==========================
+  Header
+  ============================-->
+  <?php include'header.php';?>
+ 
+
+  <!--==========================
+    Intro Section
+  ============================-->
+  <section id="intro" class="clearfix">
+    <div class="container">
+
+      <div class="intro-img">
+        <img src="img/intro-img.svg" alt="" class="img-fluid">
+      </div>
+
+      <div class="intro-info">
+        <h2>We provide<br><span>solutions</span><br>for your STUDY!</h2>
+        <div>
+          <a href="#about" class="btn-get-started scrollto">Get Started</a>
+          <!-- <a href="#universities" class="btn-universities scrollto">Universities</a>  -->
+        </div>
+      </div>
+
+    </div>
+  </section> <!-- #intro -->
+
+  <main id="main">
+
+
+
+   <!--  ==============================
+     University Section
+    =============================== -->
+    <section id="University" class="section-bg">
+
+      <div class="container">
+        <!--  <div class="section-header">
+          <h3>Universities</h3>
+          <p>Here we are family of four main universities of the Chattishgarh.</p>
+        </div> -->
+
+
+
+        <div class="section-header">
+          <h3 style="font-size:47px; padding-top: 10px;">University</h3>
+          <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque dere santome nida.</p>
+        </div>
+
+        <div class="row no-gutters University-wrap clearfix wow fadeInUp">
+
+          <div class="col-lg-3 col-md-4 col-xs-6"><a href="http://www.prsuuniv.in">            
+            <div class="University-logo">
+              <img src="img/clients/PTRSU.jpg" class="img-fluid" alt="">
+            </div></a>
+
+          </div>
+          
+          <div class="col-lg-3 col-md-4 col-xs-6"><a href="http://ddugu.ac.in/defaulthome.aspx">
+            <div class="University-logo">
+              <img src="img/clients/ddu.png" class="img-fluid" alt="">
+            </div></a>
+          </div>
+        
+          <div class="col-lg-3 col-md-6 col-xs-6"><a href="https://csvtu.ac.in">
+            <div class="University-logo">
+              <img src="img/clients/CSVTU.jpg" class="img-fluid" alt="" >
+            </div></a>
+          </div>
+          
+          <div class="col-lg-3 col-md-4 col-xs-6"><a href="http://www.new.ggu.ac.in">
+            <div class="University-logo">
+              <img src="img/clients/Ggu.png" class="img-fluid" alt="" style="max-width: 80%;">
+            </div></a>
+          </div>
+          
+          <div class="col-lg-3 col-md-4 col-xs-6">
+            <div class="University-logo">             
+                <p><b><h1 style="color:  #004a99; padding-left: 30px;">PTRSU</h1></b></p>
+            </div>
+          </div>
+        
+          <div class="col-lg-3 col-md-4 col-xs-6">
+            <div class="University-logo" >
+             
+               <p><b><h1 style="color: #004a99; padding-left: 50px;">DDU</h1></b></p>
+            </div>
+          </div>
+          
+          <div class="col-lg-3 col-md-4 col-xs-6">
+            <div class="University-logo">
+             
+              <p><b><h1 style="color:#004a99; padding-left: 50px;">CSVTU</h1></b></p>
+            </div>
+          </div> 
+         
+          <div class="col-lg-3 col-md-4 col-xs-6">
+            <div class="University-logo">
+              <p><b><h1 style="color: #004a99; padding-left: 70px;">GGU</h1></b></p>
+            </div>
+          </div>
+
+        </div> 
+         <!-- <div class="row justify-content-center">
+          <div class="col-lg-8">  
+
+            <div class="owl-carousel testimonials-carousel wow fadeInUp">
+    
+              <div class="testimonial-item">
+                <img src="img/test1.jpg" class="testimonial-img" alt="" style="width: 120px;
+                    border-radius: 50%;
+                    border: 4px solid #fff;
+                    float: left;">
+                <h3>Dikesh Sahu</h3>
+                <h4>Student</h4>
+                <p>
+                   I love the idea of the find my way Essex app however, its not very easy to use or understand unfortunately.
+                </p>
+              </div>
+    
+              <div class="testimonial-item">
+                <img src="img/test2.jpg" class="testimonial-img" alt="" style="width: 120px;
+                      border-radius: 50%;
+                      border: 4px solid #fff;
+                      float: left;">
+                <h3>Madhu Pandey</h3>
+                <h4>Freelancer</h4>
+                <p>
+                  The curriculum is well-structured, with a focus on hands-on learning and real-world applications. The university also offers a diverse community, with students from.
+                </p>
+              </div>
+    
+              <div class="testimonial-item">
+                <img src="img/test3.jpg" class="testimonial-img" alt="" style="width: 120px;
+                      border-radius: 50%;
+                      border: 4px solid #fff;
+                      float: left;">
+                <h3>Ritesh Sahu</h3>
+                <h4>Accountant</h4>
+                <p>
+                  The university is supportive in having the resources, network and different areas of specialities available to aid individual learning and achievements. I feel very supported when faced with academical challenges
+                </p>
+              </div>
+    
+              <div class="testimonial-item">
+                <img src="img/reena.jpg" class="testimonial-img" alt="" style="width: 120px;
+                    border-radius: 50%;
+                    border: 4px solid #fff;
+                    float: left;">
+                <h3>Reena Gavel</h3>
+                <h4>Web designer</h4>
+                <p>
+                   I also particularly love the accessibility of study materials through various e-Learning platforms that come with the degree such as O'Reilly, Kortext along with many others.
+                </p>
+              </div>
+    
+              <div class="testimonial-item">
+                <img src="img/boy.jpg" class="testimonial-img" alt="" style="width: 120px;
+                    border-radius: 50%;
+                    border: 4px solid #fff;
+                    float: left;">
+                <h3>Khilendra Sahu</h3>
+                <h4>Web Developer</h4>
+                <p>
+                  I love thé plateform and the interface, I think the facilities are really nice however I think there could be an improvement in websites styles . thank you.
+                </p>
+              </div>
+
+            </div>
+
+          </div> 
+        </div> -->
+
+
+      </div>
+
+    </section>
+  </main>
+
+ <?php 
+
+  include'footer.php';
+
+  ?>
+ 
+
+  <!-- - ----------footer end------------- --> -->
+
+  <!-- JavaScript Libraries -->
+  <script src="lib/jquery/jquery.min.js"></script>
+  <script src="lib/jquery/jquery-migrate.min.js"></script>
+  <script src="lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="lib/easing/easing.min.js"></script>
+  <script src="lib/mobile-nav/mobile-nav.js"></script>
+  <script src="lib/wow/wow.min.js"></script>
+  <script src="lib/waypoints/waypoints.min.js"></script>
+  <script src="lib/counterup/counterup.min.js"></script>
+  <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+  <script src="lib/isotope/isotope.pkgd.min.js"></script>
+  <script src="lib/lightbox/js/lightbox.min.js"></script>
+  <!-- Contact Form JavaScript File -->
+  <script src="contactform/contactform.js"></script>
+
+  <!-- Template Main Javascript File -->
+  <script src="js/main.js"></script>
+
+</body>
+</html>
+<!-- img/favicon.png -->
---
+++ univer_ori1.php
@@ -0,0 +1,71 @@
+  University Section
+    ============================-->
+     <section id="University" class="section-bg">
+
+      <div class="container-fluid">
+
+        <div class="section-header">
+          <h3>University</h3>
+          <p>we have gathered together more tan two universities wic is shown below in rows . </p>
+        </div>
+
+        <div class="row no-gutters University-wrap clearfix wow fadeInUp">
+
+          <div class="col-lg-3 col-md-4 col-xs-6">
+            <div class="University-logo">
+              <img src="img/clients/CSVTU.jpg" class="img-fluid" alt="">
+            </div>
+          </div>
+          
+          <div class="col-lg-3 col-md-4 col-xs-6">
+            <div class="University-logo">
+              <img src="img/clients/ddu.png" class="img-fluid" alt="">
+            </div>
+          </div>
+        
+          <div class="col-lg-3 col-md-4 col-xs-6">
+            <div class="University-logo">
+              <img src="img/clients/PTRSU.jpg" class="img-fluid" alt="">
+            </div>
+          </div>
+          
+          <div class="col-lg-3 col-md-4 col-xs-6">
+            <div class="University-logo">
+              <img src="img/clients/Ggu.png" class="img-fluid" alt="">
+            </div>
+          </div>
+          
+          <div class="col-lg-3 col-md-4 col-xs-6">
+            <div class="University-logo">
+            <!-- <img src="img/clients/client-5.png" class="img-fluid" alt="">  -->
+                <p><b><h1 style="color: #004a99; padding-left: 30px;">CSVTU</h1></b></p>
+            </div>
+          </div>
+        
+          <div class="col-lg-3 col-md-4 col-xs-6">
+            <div class="University-logo">
+              <!-- <img src="img/clients/client-6.png" class="img-fluid" alt="">  -->
+              <p><b><h1 style="color:#004a99; padding-left: 30px;">DDU</h1> #004a99; padding-left: 30px;</b></p>
+            </div>
+          </div>
+          
+          <div class="col-lg-3 col-md-4 col-xs-6">
+            <div class="University-logo">
+             <!--  <img src="img/clients/client-7.png" class="img-fluid" alt=""> -->
+              <p><b><h1 style="color:#004a99; padding-left: 50px;">PTRSU</h1></b></p>
+            </div>
+          </div> 
+         
+          <div class="col-lg-3 col-md-4 col-xs-6">
+            <div class="University-logo">
+             <!--  <img src="img/clients/client-8.png" class="img-fluid" alt=""> --> 
+               <p><b><h1 style="color: #004a99; padding-left: 70px;">GGU</h1></b></p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+    </section>  
+ 
